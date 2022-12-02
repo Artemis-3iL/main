6d7fa36b2bfc64855bd5cf4e7e3b969e7c0d019d
SolMat04 <83272658+SolMat04@users.noreply.github.com>
2022-12-02 01:26:58 +0100

    backup Co-authored-by: Ruru-TheBigY <Ruru-TheBigY@users.noreply.github.com> Co-authored-by: lou.hita04@gmail.com <lou.hita04@gmail.com>

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 window.onload = function(){
-    // Récuperr rep_labels
+    // Récuperer rep_labels
+    liste_reponses_input = [
+        document.getElementById("rep1"),
+        document.getElementById("rep2"),
+        document.getElementById("rep3"),
+        document.getElementById("rep4")
+    ];
     liste_reponses_labels = [
         document.getElementById("rep1_label"),
         document.getElementById("rep2_label"),
         document.getElementById("rep3_label"),
         document.getElementById("rep4_label")
     ];
+    reponse_choisie = 0;
     question_n = 0;
     document.getElementById("questionnaire_titre").textContent = liste_question[question_n];
     for (let i = 0; i < liste_question.length; i++) {
@@ -16,13 +23,18 @@ var question_n;
 var liste_reponses_labels ;
 var liste_question = ["Question A","Question B","Question C","Question D"]
 var liste_reponses = [["Réponse A-1","Réponse A-2","Réponse A-3","Réponse A-4"],["Réponse B-1","Réponse B-2","Réponse B-3","Réponse B-4"],["Réponse C-1","Réponse C-2","Réponse C-3","Réponse C-4"],["Réponse D-1","Réponse D-2","Réponse D-3","Réponse D-4"]];
+var reponse_choisie;
 function choix(n){
-    alert(n);
+    reponse_choisie = n;
 }
 function valider(){
-    question_n++;
-    document.getElementById("questionnaire_titre").textContent = liste_question[question_n];
-    for (let i = 0; i < liste_question.length; i++) {
-        liste_reponses_labels[i].textContent = liste_reponses[question_n][i];
+    if(reponse_choisie != 0){
+        question_n++;
+        document.getElementById("questionnaire_titre").textContent = liste_question[question_n];
+        for (let i = 0; i < liste_question.length; i++) {
+            liste_reponses_labels[i].textContent = liste_reponses[question_n][i];
+        }
+        liste_reponses_input[reponse_choisie-1].checked = false;
+        reponse_choisie = 0;
     }
 }
